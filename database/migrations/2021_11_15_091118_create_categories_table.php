f03f76ecc3fefab3c5157de895f89c85b756892f
Emre Bozkurt <emre75kurt@gmail.com>
2021-11-15 12:28:24 +0300

    categories table and model created, sluggable package added.

---
+++ database/migrations/2021_11_15_091118_create_categories_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('parent_id')->nullable()->constrained('categories');
+            $table->string('name');
+            $table->string('slug');
+            $table->longText('description');
+            $table->string('image')->nullable();
+            $table->tinyInteger('status')->default(0);
+            $table->tinyInteger('popular')->default(0);
+            $table->string('meta_title')->nullable();
+            $table->string('meta_desc')->nullable();
+            $table->string('meta_keywords')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categories');
+    }
+}
